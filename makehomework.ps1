1dc3560b0bc9524ec1754bcbba2d77f53b42fc8d
LiangA <10252988+LiangA@users.noreply.github.com>
2020-03-03 15:06:38 -0500

    powershell file

---
+++ makehomework.ps1
@@ -0,0 +1,14 @@
+$email = Read-Host -Prompt "input your email"
+
+$series =  Read-Host -Prompt "input homework series"
+
+$problems_input = Read-Host -Prompt 'input problem numbers'
+
+$problems = -split $problems_input 
+
+foreach ($problem in $problems) {
+$p = $problem -split "\."
+echo $p.getType()
+$name = '.\{0}_hw_{1}_{2}_{3}.py' -f $email, $series, $p[0], $p[1] 
+New-Item -Path '.' -Name $name -ItemType file -Force
+}
